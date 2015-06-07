05533e4cbf4442d77110170d7e0fe3d0ab1f5cc1
zhangqinghua <the.patron.saint.of.science@gmail.com>
2015-06-08 01:18:53 +0800

    TTT

---
+++ src/HelloWorld.java
@@ -0,0 +1,17 @@
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("HelloWorld");
+	}
+	
+	public void test(){
+		
+	}
+	public void test2(){
+		
+	}
+	
+	public void test3(){
+		
+	}
+}
